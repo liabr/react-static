8351a171d1ecca4bede6a2ec4739dc483753f5b4
Lijo Abraham <lijo.abraham.work@gmail.com>
2023-09-21 07:27:55 -0500

    Updates

---
+++ precache-manifest.642f9eb0f26599dd76ad430d04a22db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a364d1c0872d67afe4",
+    "url": "/react-static/static/css/main.6795e541.chunk.css"
+  },
+  {
+    "revision": "57a364d1c0872d67afe4",
+    "url": "/react-static/static/js/main.57a364d1.chunk.js"
+  },
+  {
+    "revision": "9f124b154c962e2b6e20",
+    "url": "/react-static/static/js/1.9f124b15.chunk.js"
+  },
+  {
+    "revision": "7df5fb54b46394961270",
+    "url": "/react-static/static/js/runtime~main.7df5fb54.js"
+  },
+  {
+    "revision": "621fcae0059b39f7797ce427a8de8d19",
+    "url": "/react-static/index.html"
+  }
+];
